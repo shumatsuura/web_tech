85149ce87eb8c5599380174af332fc7c603d586d
shumatsuura <shunsuke.mtr@gmail.com>
2020-01-13 21:44:12 +0900

    submit2

--- test.rb
+++ test.rb
@@ -13,4 +13,8 @@ server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'ex.html.erb')
+server.mount('/data1.cgi', WEBrick::HTTPServlet::CGIHandler, 'data1.rb')
+server.mount('/data2.cgi', WEBrick::HTTPServlet::CGIHandler, 'data2.rb')
+
 server.start
